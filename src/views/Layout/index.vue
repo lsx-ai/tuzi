009ade204aacfc4daa83b86843a7b1dd6aa7ecd5
lsx <2391847188@qq.com>
2023-06-02 21:49:12 +0800

    layout 静态资源引入

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,7 @@
 <script setup>
-import LayoutNav from '../components/LayoutNav.vue'
-import LayoutHeader from '../components/LayoutHeader.vue'
-import LayoutFooter from '../components/LayoutFooter.vue'
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
 <template>
